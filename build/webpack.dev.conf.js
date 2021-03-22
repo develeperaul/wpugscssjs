8fc6b736194757571a080af009cad2386491f195
Ralfik <developeraul@gmail.com>
2021-03-22 18:02:51 +0300

    сборка webpack 4

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,22 +1,29 @@
-const  webpack = require('webpack');
+
+const webpack = require('webpack');
+
+
 const { merge } = require('webpack-merge');
+
 const baseWebpackConfig = require('./webpack.base.conf');
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-mode: 'development',
-devServer: {
-    overlay: true,
-    open: true,
-    port: 8081
-},
-
-plugins: [
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: baseWebpackConfig.externals.paths.dist,
+    port: 8081,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  plugins: [
     new webpack.SourceMapDevToolPlugin({
-        filename: '[filename].js.map'
+      filename: '[file].map'
     })
-]
-})
+  ]
+});
 
-module.exports = new Promise((resolve, reject) =>{
-resolve(devWebpackConfig);
-})
+module.exports = new Promise((resolve, reject) => {
+  resolve(devWebpackConfig)
+});
